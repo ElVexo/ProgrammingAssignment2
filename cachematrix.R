0ee8a2b233330b92bce4fc3887f4ba2a6109bf6c
ElVexo <bryanguilliams@gmail.com>
2015-08-23 13:45:28 -0500

    Added code to functions
    First pass at creating makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions demonstrate R's ability to cache values rather than recomputing them, when appropriate.
+## In this example, the inverse of a matrix is computed using the solve() function.  If an unchanged matrix is passed to the function,
+## the cached value will be returned rather than re-computing the same value.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function takes a matrix as an argument.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
